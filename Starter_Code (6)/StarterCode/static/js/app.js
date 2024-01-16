6709c09735e8111f127c1202841b164baf0603ee
suhasinijam <suhasiniar@gmail.com>
2024-01-16 13:27:29 -0600

    code

--- Starter_Code (6)/StarterCode/static/js/app.js
+++ Starter_Code (6)/StarterCode/static/js/app.js
@@ -5,3 +5,9 @@ let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1
 d3.json(url).then(function(data) => {
     console.log(data);
 });
+//initialzing dashboard
+function initializeDashboard() {
+
+//using d3 to select dashboard
+let dropdown = d3.select("#selDataset");
+}
