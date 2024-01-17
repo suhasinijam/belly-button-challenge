f2c4b1f109a41a3bb7095fc1c3e4a91a8bf5543e
suhasinijam <suhasiniar@gmail.com>
2024-01-16 20:43:45 -0600

    code

--- Starter_Code (6)/StarterCode/static/js/app.js
+++ Starter_Code (6)/StarterCode/static/js/app.js
@@ -5,9 +5,23 @@ let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1
 d3.json(url).then(function(data) {
     console.log(data);
 });
-//initialzing dashboard
-function initializeDashboard() {
 
-//using d3 to select dashboard
-let dropdown = d3.select("#selDataset");
-}
+//initialzing dashboard
+function init(){
+    //drop down selection
+    let dropdown = d3.select("#selDataset");
+    //accessing data using d3
+    d3.json(url).then((data) => {
+        let sample_ids = data.names;
+        console.log(sample_ids);
+        for (id of sample_ids){
+            dropdown.append("option").attr("value", id).text(id);
+        };
+    //store the first sample for display
+    let first_entry = sample_ids[0];
+    console.log(first_entry);
+    makeBar(first_entry);
+    makeBubble(first_entry);
+    makeDemographics(first_entry);
+    }); //end of d3 access
+};
